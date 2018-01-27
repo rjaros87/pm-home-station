b8e9ae944ba4a5d3a66beec293379a696a4c7589
Sanchin <radoslaw.gabiga@gmail.com>
2018-01-27 19:03:16 +0100

    [A] Percentage display

--- android/src/main/java/pmstation/SettingsFragment.java
+++ android/src/main/java/pmstation/SettingsFragment.java
@@ -6,13 +6,17 @@
 
 package pmstation;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource
@@ -26,5 +30,32 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             view.setBackgroundColor(getResources().getColor(android.R.color.background_light));
         }
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        getPreferenceScreen().getSharedPreferences()
+                             .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("main_value")) {
+            Log.d("SettingsScreen", "main_value pref changed");
+            Fragment valuesFragment =
+                    getActivity().getSupportFragmentManager().findFragmentByTag(MainActivity.VALUES_FRAGMENT);
+            if (valuesFragment != null) {
+                ((ValuesFragment) valuesFragment).changeMainValue();
+            }
+//            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+        }
     }
 }
--- android/src/main/java/pmstation/ValuesFragment.java
+++ android/src/main/java/pmstation/ValuesFragment.java
@@ -7,7 +7,9 @@
 package pmstation;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -28,14 +30,26 @@ import pmstation.plantower.Settings;
 
 public class ValuesFragment extends Fragment implements IPlanTowerObserver {
     private static final String TAG = "ValuesFragment";
+    SharedPreferences.OnSharedPreferenceChangeListener spChanged = (sharedPreferences, key) -> {
+
+    };
     private CardView pm1Card;
     private CardView pm25Card;
     private CardView pm10Card;
     private TextView pm1;
+    private TextView pm1Secondary;
     private TextView pm25;
+    private TextView pm25Secondary;
+    private TextView pm25Unit;
+    private TextView pm25SecondaryUnit;
     private TextView pm10;
+    private TextView pm10Secondary;
+    private TextView pm10Unit;
+    private TextView pm10SecondaryUnit;
+
     private TextView time;
     private ParticulateMatterSample currentValue;
+    private SharedPreferences preferences;
 
     public ValuesFragment() {
         // Required empty public constructor
@@ -53,9 +67,19 @@ public class ValuesFragment extends Fragment implements IPlanTowerObserver {
         pm1Card = view.findViewById(R.id.pm1_card);
         pm25Card = view.findViewById(R.id.pm25_card);
         pm10Card = view.findViewById(R.id.pm10_card);
-        pm1 = pm1Card.findViewById(R.id.pm_value);
-        pm25 = pm25Card.findViewById(R.id.pm_value);
-        pm10 = pm10Card.findViewById(R.id.pm_value);
+        pm1 = pm1Card.findViewById(R.id.pm_main_value);
+        pm1Secondary = pm1Card.findViewById(R.id.pm_secondary_value);
+        pm10SecondaryUnit = pm1Card.findViewById(R.id.pm_secondary_unit);
+        pm1Secondary.setVisibility(View.GONE);
+        pm10SecondaryUnit.setVisibility(View.GONE);
+        pm25 = pm25Card.findViewById(R.id.pm_main_value);
+        pm25Unit = pm25Card.findViewById(R.id.pm_main_unit);
+        pm25SecondaryUnit = pm25Card.findViewById(R.id.pm_secondary_unit);
+        pm25Secondary = pm25Card.findViewById(R.id.pm_secondary_value);
+        pm10 = pm10Card.findViewById(R.id.pm_main_value);
+        pm10Secondary = pm10Card.findViewById(R.id.pm_secondary_value);
+        pm10Unit = pm10Card.findViewById(R.id.pm_main_unit);
+        pm10SecondaryUnit = pm10Card.findViewById(R.id.pm_secondary_unit);
         ((TextView) pm1Card.findViewById(R.id.pm_label)).setText(R.string.pm1);
         ((TextView) pm25Card.findViewById(R.id.pm_label)).setText(R.string.pm25);
         ((TextView) pm10Card.findViewById(R.id.pm_label)).setText(R.string.pm10);
@@ -79,6 +103,8 @@ public class ValuesFragment extends Fragment implements IPlanTowerObserver {
         super.onActivityCreated(savedInstanceState);
         MainActivity activity = (MainActivity) getActivity();
         activity.getSensor().addValueObserver(this);
+        preferences = PreferenceManager.getDefaultSharedPreferences(activity);
+        changeMainValue();
     }
 
     @Override
@@ -98,12 +124,14 @@ public class ValuesFragment extends Fragment implements IPlanTowerObserver {
         activity.runOnUiThread(() -> {
             pm1.setText(String.format(Locale.getDefault(), "%d", sample.getPm1_0()));
             pm25.setText(String.format(Locale.getDefault(), "%d", sample.getPm2_5()));
+            pm25Secondary.setText(String.format(Locale.getDefault(), "%d", Math.round(sample.getPm2_5() / 25f * 100)));
             AQIColor pm25Color = AQIColor.fromPM25Level(sample.getPm2_5());
             pm1Card.setCardBackgroundColor(
                     ColorUtils.setAlphaComponent(pm25Color.getColor(), 136));
             pm25Card.setCardBackgroundColor(
                     ColorUtils.setAlphaComponent(pm25Color.getColor(), 136));
             pm10.setText(String.format(Locale.getDefault(), "%d", sample.getPm10()));
+            pm10Secondary.setText(String.format(Locale.getDefault(), "%d", Math.round(sample.getPm10() / 25f * 100)));
             pm10Card.setCardBackgroundColor(
                     ColorUtils.setAlphaComponent(AQIColor.fromPM10Level(sample.getPm10()).getColor(), 136));
             time.setText(Settings.dateFormat.format(sample.getDate()));
@@ -114,5 +142,30 @@ public class ValuesFragment extends Fragment implements IPlanTowerObserver {
     public void onSaveInstanceState(@NonNull Bundle outState) {
         outState.putSerializable("value", currentValue);
         super.onSaveInstanceState(outState);
+    }
+
+    public void changeMainValue() {
+        int main_value = Integer.parseInt(preferences.getString("main_value", "1"));
+        TextView tmpView = pm25;
+        pm25 = pm25Secondary;
+        pm25Secondary = tmpView;
+        tmpView = pm10;
+        pm10 = pm10Secondary;
+        pm10Secondary = tmpView;
+        switch (main_value) {
+            case 2:
+                pm25Unit.setText(R.string.percent);
+                pm25SecondaryUnit.setText(R.string.unit);
+                pm10Unit.setText(R.string.percent);
+                pm10SecondaryUnit.setText(R.string.unit);
+                break;
+            case 1:
+                pm25Unit.setText(R.string.unit);
+                pm25SecondaryUnit.setText(R.string.percent);
+                pm10Unit.setText(R.string.unit);
+                pm10SecondaryUnit.setText(R.string.percent);
+            default:
+                break;
+        }
     }
 }
