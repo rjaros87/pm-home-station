9fa64abce2ec3463bda1650b5e9848c1fabbaf9a
Jerzy Lukjaniec <df@dflab.net>
2018-02-22 19:19:02 +0100

    [iOS] Keep Screen On setting added

--- iOS/PMMonitor/AppDelegate.swift
+++ iOS/PMMonitor/AppDelegate.swift
@@ -34,7 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        application.isIdleTimerDisabled = UserDefaults.standard.bool(forKey: "keepScreenOn")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
