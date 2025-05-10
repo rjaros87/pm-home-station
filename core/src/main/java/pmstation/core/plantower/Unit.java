8bcf2030774e88f770f1853ebbef1bcb0eb18c75
Piotr Skowronek <piotr@skowro.net>
2025-05-10 18:52:21 +0200

    Fixed units, as HA (via MQTT) complains that they are not as expected (#45)

--- core/src/main/java/pmstation/core/plantower/Unit.java
+++ core/src/main/java/pmstation/core/plantower/Unit.java
@@ -1,10 +1,10 @@
 package pmstation.core.plantower;
 
 public enum Unit {
-    PARTICULATE_MATTER("\u03BCg/m\u00B3"),
-    HCHO_UG("\u03BCg/m\u00B3"),
-    HCHO_MG("mg/m\u00B3"),
-    TEMPERATURE("\u2103"),
+    PARTICULATE_MATTER("\u00B5g/m\u00B3"), // µg/m³
+    HCHO_UG("\u00B5g/m\u00B3"), // µg/m³
+    HCHO_MG("mg/m\u00B3"), // mg/m³
+    TEMPERATURE("\u00B0C"), // °C
     HUMIDITY("%"),
     ;
 
