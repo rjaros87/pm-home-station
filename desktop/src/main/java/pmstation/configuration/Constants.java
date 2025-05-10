20be8e2eac3a2901bf15865d29b36bc33e8fe2db
Piotr Skowronek <piotr@skowro.net>
2025-05-10 19:33:38 +0200

    pm-home-station desktop app to still use java17 plus make it run on macos apple silicon (#46)

--- desktop/src/main/java/pmstation/configuration/Constants.java
+++ desktop/src/main/java/pmstation/configuration/Constants.java
@@ -14,7 +14,7 @@ import pmstation.core.plantower.Unit;
 public class Constants {
     
     public static final String PROJECT_NAME = "pm-home-station";
-    public static final String VERSION = "1.3.0-alpha";
+    public static final String VERSION = "1.3.1-alpha";
     public static final String PROJECT_URL = "https://github.com/rjaros87/pm-home-station";
     public static final Locale DEFAULT_LOCALE = Locale.getDefault(); 
     public static final String DEFAULT_ICON = "app-icon.png";
